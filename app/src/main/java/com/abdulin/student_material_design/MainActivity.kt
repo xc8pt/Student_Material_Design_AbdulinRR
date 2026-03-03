b1ea08be44b0864974c1ab1c205010fea6efcfb5
xc8pt <brolike636@gmail.com>
2026-03-03 16:45:59 +0300

    Step 10: Top App Bar added

--- app/src/main/java/com/abdulin/student_material_design/MainActivity.kt
+++ app/src/main/java/com/abdulin/student_material_design/MainActivity.kt
@@ -16,11 +16,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
@@ -104,7 +108,11 @@ fun StudentItem(
 
 @Composable
 fun StudentApp() {
-    Scaffold { it ->
+    Scaffold(
+        topBar = {
+            StudentTopAppBar()
+        }
+    ) { it ->
         LazyColumn(contentPadding = it) {
             items(students) {
                 StudentItem(
@@ -118,6 +126,33 @@ fun StudentApp() {
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StudentTopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .size(dimensionResource(R.dimen.image_size))
+                        .padding(dimensionResource(R.dimen.padding_small)),
+                    painter = painterResource(R.drawable.ic_student_logo),
+                    contentDescription = null
+                )
+                Text(
+                    text = stringResource(R.string.app_name),
+                    style = MaterialTheme.typography.displayLarge
+                )
+            }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = modifier
+    )
+}
 @Preview
 @Composable
 fun StudentDarkThemePreview() {
